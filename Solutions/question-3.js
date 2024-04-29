dc0e7accbf2d506f5cda351ee6f1f5045b84319a
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-04-29 21:31:31 +0300

    question-3

---
+++ Solutions/question-3.js
@@ -0,0 +1,11 @@
+// Function to print all three-digit numbers in decreasing order
+function printThreeDigitNumbers() {
+    for (let num = 999; num >= 100; num--) {
+        console.log(num);
+    }
+}
+
+// Call the function to print the numbers
+printThreeDigitNumbers();
+
+console.log(printThreeDigitNumbers)
